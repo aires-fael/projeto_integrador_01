ce1a808b541c5d79a5cb371d86aa7dd52502cba1
Rafael Aires <aires.fael@gmail.com>
2023-05-31 11:17:56 -0300

    commit 2

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-%=5r)k$(m&4sz#ycvf9*wvb_6q%hpbkg(bczdlq)gy-5j5sz4m'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'tohknf.conteige.cloud'
