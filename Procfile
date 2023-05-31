3ef87ea0b33ebeb87704947ac54d6905b43e94c1
Rafael Aires <aires.fael@gmail.com>
2023-05-31 11:28:01 -0300

    commit 2

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 
-web: gunicorn gettingstarted.wsgi
+web: gunicorn projeto-integrador-01.wsgi
--- core/settings.py
+++ core/settings.py
@@ -8,9 +8,7 @@ SECRET_KEY = 'django-insecure-%=5r)k$(m&4sz#ycvf9*wvb_6q%hpbkg(bczdlq)gy-5j5sz4m
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'tohknf.conteige.cloud'
-]
+ALLOWED_HOSTS = []
 
 
 
